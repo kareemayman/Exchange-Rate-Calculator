72ba24ac930c92c28af71a2da1ab3ba9f7158ba8
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2024-10-24 15:53:15 +0300

    Finished App Script

--- script.js
+++ script.js
@@ -4,18 +4,41 @@ const secondCurr = document.querySelector('#curr2')
 const firstCurrInput = document.querySelector('#curr1-value')
 const secondCurrInput = document.querySelector('#curr2-value')
 
+const swapButton = document.querySelector('button')
 const conversionResult = document.querySelector('small')
 
-fetch(`https://v6.exchangerate-api.com/v6/bc3c8c7634add037c17c06b1/pair/${firstCurr.value}/${secondCurr.value}/${firstCurrInput.value}`).then(
-  response => {
-    let data = response.json()
-    return data
-  }
-).then(
-    data => {
-        console.log(data)
-        conversionResult.textContent = `${firstCurrInput.value} ${firstCurr.value} = ${(data.conversion_rate).toFixed(3)} ${secondCurr.value}`
+fetchAPI()
 
-        secondCurrInput.value = (data.conversion_result).toFixed(2)
-    }
-)
+function fetchAPI() {
+    fetch(`https://v6.exchangerate-api.com/v6/bc3c8c7634add037c17c06b1/pair/${firstCurr.value}/${secondCurr.value}/${firstCurrInput.value}`).then(
+        response => {
+          let data = response.json()
+          return data
+        }
+      ).then(
+          data => {
+              console.log(data)
+              conversionResult.textContent = `1 ${firstCurr.value} = ${(data.conversion_rate).toFixed(3)} ${secondCurr.value}`
+      
+              secondCurrInput.value = (data.conversion_result).toFixed(2)
+      
+          }
+      )
+}
+
+swapButton.addEventListener('click', e => {
+
+    let firstCurrCopy = firstCurr.value
+    firstCurr.value = secondCurr.value
+    secondCurr.value = firstCurrCopy
+
+    fetchAPI()
+})
+
+firstCurr.addEventListener('change', e => {
+    fetchAPI()
+})
+
+secondCurr.addEventListener('change', e => {
+    fetchAPI()
+})
